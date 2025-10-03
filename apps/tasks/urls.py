cb40134bda47e04ab829892b2438d6ec7c4b9685
Zakharov_Rostislav <r.z.8102.80@gmail.com>
2025-10-03 12:48:14 +0400

    fix(apps.tasks.urls.py): fix admin viewset bug

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.tasks.views import UserTaskViewSet
+from apps.tasks.views import UserTaskViewSet, AdminTaskViewSet
 
 router = DefaultRouter()
 router.register(r'user-tasks', UserTaskViewSet, basename='user-task')
-router.register(r'admin-tasks', UserTaskViewSet, basename='admin-task')
+router.register(r'admin-tasks', AdminTaskViewSet, basename='admin-task')
 
 app_name = 'tasks'
 urlpatterns = [
